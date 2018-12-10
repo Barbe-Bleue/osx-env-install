b2f6b636fa70b7d132b2600295f8e20481acdb58
Guigui <screy@icloud.com>
2018-12-10 11:14:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,54 +10,63 @@ echo -e "${RED}LET'S${NC} ${GREEN}GO${NC} ${YELLOW}MA${NC} ${PURPLE}BOIIII${NC}"
 # check if homebrew is installed
 if test ! $(which brew); then
 	echo -e "${GREEN}Install homebrew...${NC}"
-  #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # update homebrew
 echo -e "${GREEN}Ppdate homebrew...${NC}"
-#brew update
+brew update
 
 # cask and mas
 echo -e "${GREEN}Install cask and mas...${NC}"
 # install mac osx app 
-#brew tap homebrew/cask-versions
+if test ! $(which cask); then
+	echo -e "${GREEN}Install cask-version...${NC}"
+	brew tap homebrew/cask-versions
+fi
 
 # install macapp from the mac app store
-#brew tap mas-cli/tap
-#brew tap-pin mas-cli/tap
+if test ! $(which mas); then
+	echo -e "${GREEN}Install mas...${NC}"
+	brew tap mas-cli/tap
+	brew tap-pin mas-cli/tap
+fi
 
 echo -e "\n${GREEN}Install homebrew packages...${NC}"
 
 # basic packages
 echo -e "\n${GREEN}Install homebrew basic packages...${NC}"
+echo -e "${PURPLE}${BASIC_PACKAGES[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}BASIC_PACKAGES${NC} ? [press ennter]) " input
 case $input in
 	[yY][eE][sS]|[yY]|'')
 		echo -e "${GREEN}Install BASIC packages...${NC}"
 		echo -e "${ORANGE}${BASIC_PACKAGES[@]}${NC}\n"
-		#brew install ${BASIC_PACKAGES[@]}
+		brew install ${BASIC_PACKAGES[@]}
 	;;
 esac
 
 # dev packages
 echo -e "\n${GREEN}Install homebrew dev package...${NC}"
+echo -e "${PURPLE}${DEV_PACKAGES[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}DEV_PACKAGES${NC} ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
 		echo -e "${GREEN}Install DEV packages...${NC}"
 		echo -e "${ORANGE}${DEV_PACKAGES[@]}${NC}\n"
-		#brew install ${DEV_PACKAGES[@]}
+		brew install ${DEV_PACKAGES[@]}
 	;;
 esac
 
 # fun packages
 echo -e "\n${GREEN}Install homebrew fun packages...${NC}"
+echo -e "${PURPLE}${FUN_PACKAGES[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}FUN_PACKAGES${NC} ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
 		echo -e "${GREEN}Install FUN packages...${NC}"
 		echo -e "${ORANGE}${FUN_PACKAGES[@]}${NC}\n"
-		#brew install ${FUN_PACKAGES[@]}
+		brew install ${FUN_PACKAGES[@]}
 	;;
 esac
 
@@ -66,6 +75,7 @@ echo -e "\n${GREEN}Install mac osx apps ${NC}"
 
 # basic mac osx apps
 echo -e "\n${GREEN}Install basic mac osx apps ${NC}"
+echo -e "${PURPLE}${BASIC_CASKS[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}BASIC_CASKS${NC} ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
@@ -74,6 +84,7 @@ case $input in
 esac
 
 # dev mac osx apps
+echo -e "${PURPLE}${DEV_CASKS[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}DEV_CASKS${NC} pour le dev web ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
@@ -81,6 +92,7 @@ case $input in
  esac
  	
 # others mac osx apps
+echo -e "${PURPLE}${OTHER_CASKS[@]}${NC}\n"
 read -r -p "$(echo -e Do you want ${RED}OTHER_CASKS${NC} ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
@@ -89,12 +101,13 @@ esac
 
 # atom packages
 if test $(which atom); then
+	echo -e "${PURPLE}${ATOM_PACKAGE[@]}${NC}\n"
 	read -r -p "$(echo -e Do you want super ${GREEN}atom-package${NC} ? [press enter])" input
 	case $input in
 		[yY][eE][sS]|[yY]|'')
 			echo -e "${ORANGE}${ATOM_PACKAGE[@]}${NC}"
 	 		echo -e "${GREEN}Install atom-packages...${NC}\n"
-	 		#apm install ${ATOM_PACKAGE[@]}
+	 		apm install ${ATOM_PACKAGE[@]}
 		;;
 	esac
 fi
@@ -105,13 +118,14 @@ if test $(which atom); then
 	case $input in
 		[yY][eE][sS]|[yY]|'')
 	 		echo -e "${PURPLE}Install atom dracula theme... mouhahaha${NC}\n"
-	 		#apm install dracula-theme
+	 		apm install dracula-theme
 	 	;;
 	esac
 fi
 
 # yarn packages
 if test $(which yarn); then
+	echo -e "${PURPLE}${YARN_PACKAGE[@]}${NC}\n"
   read -r -p "$(echo -e Do you want ${CYAN}yarn packages${NC} ? [press enter])" input
 	case $input in
 		[yY][eE][sS]|[yY]|'')
@@ -125,7 +139,7 @@ echo -e "\n${GREEN}Install alias...${NC}"
 read -r -p "$(echo -e I have ${GREEN}super${NC} ${PURPLE}cool${NC} ${YELLOW}aliases${NC} if you say ${GREEN}YES${NC} I will ${RED}DELETE${NC} your ${ORANGE}.bash_profile${NC} ? [press enter])" input
 case $input in
 	[yY][eE][sS]|[yY]|'')
- 		#echo "$(cat bash_profile.txt)" > ~/.bash_profile
+ 		echo "$(cat bash_profile.txt)" > ~/.bash_profile
  	;;
 esac
 
@@ -137,7 +151,7 @@ read -r -p "$(echo -e Create ${GREEN}~/Sites/${NC} folder ? [press enter])" inpu
 case $input in
 	[yY][eE][sS]|[yY]|'')
 		echo -e "Create {GREEN}~/Sites${NC} folder OSX...\n"
- 		#mkdir ~/Sites
+ 		mkdir ~/Sites
  	;;
 esac
 
@@ -151,13 +165,13 @@ if test $(which git); then
 			read -r -p "$(echo -e Your github ${RED}pseudo${NC} ? )" input
 			if [ "$input" != "" ]; then
 				echo -e "${GREEN}$input${NC}\n"
-		   	#git config --global user.name "$input"
+		   	git config --global user.name "$input"
 			fi
 			
 			read -r -p "$(echo -e Your github ${RED}email${NC} ? )" input
 			if [ "$input" != "" ]; then
 				echo -e "${GREEN}$input${NC}\n"
-		   	#git config --global user.email "$input"
+		   	git config --global user.email "$input"
 			fi
 	 	;;
 	esac
@@ -166,7 +180,7 @@ if test $(which git); then
 	case $input in
 		[yY][eE][sS]|[yY]|'')
 			echo -e "${GREEN}Add git alias...{NC}\n"
-	 		#echo "$(cat alias_git.txt)" >> ~/.gitconfig
+	 		echo "$(cat alias_git.txt)" >> ~/.gitconfig
 	 	;;
 	esac
 fi
