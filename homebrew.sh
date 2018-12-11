31f0fadab3c2beefe9e86974c09d922b68361e8b
Barbe-Bleue <screy@icloud.com>
2018-12-11 10:37:51 +0100

    create homebrew package file

---
+++ homebrew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+BASIC_PACKAGES=(
+	cask
+	htop 
+	curl 
+	ncdu 
+	wget 
+	bash-completion 
+	screenfetch 
+	imagesnap 
+	ffmpeg 
+	youtube-dl 
+	exa 
+	mas
+	mps-youtube
+)
+
+DEV_PACKAGES=(
+	yarn 
+	composer 
+	git 
+	gpg 
+	npm
+)
+
+FUN_PACKAGES=(
+	cmatrix 
+	cowsay 
+	fortune 
+	asciiquarium 
+	toilet 
+	lolcat 
+	sl
+)
