366340c474b2e8ef0ca8206eddf6f42862a7f9ed
Barbe-Bleue <screy@icloud.com>
2018-12-11 10:37:57 +0100

    create homebrew cask package file

---
+++ homebrew-cask.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+BASIC_CASKS=(
+  appcleaner 
+	macdown 
+	copyq 
+	iina 
+	spectacle
+)
+
+DEV_CASKS=(
+  atom 
+	visual-studio-code 
+	sequel-pro 
+	mongodb-compass-community 
+	postman 
+	coteditor
+	iterm2 
+	hyper 
+	upterm
+)
+
+OTHER_CASKS=(
+	min
+	slack
+)
