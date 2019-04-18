94f37190eb6924ec38a81ce1a6d7a17d63ab22fe
barbe-bleue <screy@icloud.com>
2019-04-18 18:44:55 +0200

    Update code.sh

--- packages/code.sh
+++ packages/code.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+CODE_PACKAGE=(
+  bmewburn.vscode-intelephense-client
+  christian-kohler.path-intellisense
+  dracula-theme.theme-dracula
+  naumovs.color-highlight
+  PKief.material-icon-theme
+)
