508c27c02b92e1ea17a27c3f4da5ea0c9c00c46f
jppaled <22444128+jppaled@users.noreply.github.com>
2020-11-29 02:02:37 +0000

    Update homebrew-cask.sh

--- packages/homebrew-cask.sh
+++ packages/homebrew-cask.sh
@@ -11,20 +11,15 @@ BASIC_CASKS=(
 DEV_CASKS=(
 	atom
 	docker
-	gas-mask
 	hyper
 	iterm2
-	mongodb-compass-community
 	postman
 	sequel-pro-nightly
 	slack
-	upterm
 	vscodium
 )
 
 OTHER_CASKS=(
-	iina
-	min
 	mpv
 	macdown
 )
